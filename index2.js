382ce280e3dea8994a7783af24b282bb12cf2142
Vidyasagar5566 <85301020+Vidyasagar5566@users.noreply.github.com>
2021-12-04 23:18:02 +0530

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,10 @@
+function checkin() {
+	var in1 = document.getElementById("input1").value;
+	var in2 = document.getElementById("input2").value;
+	document.getElementById("out1").textContent = "Hello " + "Mr./Ms." + in1 + "!";
+	document.getElementById("out2").textContent = "Welcome to the NIT CALICUT Website!!";
+	document.getElementById("out3").textContent = "Your Roll Number is " + in2;
+	
+
+
+}
