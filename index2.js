68692e7f7c81331a27390e7e23901b43dcd76107
Vidyasagar5566 <85301020+Vidyasagar5566@users.noreply.github.com>
2021-12-05 16:39:05 +0530

    Update index2.js

--- index2.js
+++ index2.js
@@ -15,23 +15,16 @@ function details() {
                    "EC":"ELECTRONICS AND COMMUNICATION ENGNEERING BRANCH",
                    "EE":"ELECTRICAL AND ELECTRONICS ENGNEERING BRANCH",
                    "ME":"MECANICAL ENGNEERING BRANCH",
-                   "CV":"CIVIL ENGNEERING BRANCH",
+                   "CE":"CIVIL ENGNEERING BRANCH",
                    "CH":"CHEMICAL ENGNEERING BRANCH",
                    "BT":"BIOTECHNOLOGY BRANCH",
                    "cs":"COMPUTER SCIENCE AND ENGNEERING BRANCH",
                    "ec":"ELECTRONICS AND COMMUNICATION ENGNEERING BRANCH",
                    "ee":"ELECTRICAL AND ELECTRONICS ENGNEERING BRANCH",
                    "me":"MECANICAL ENGNEERING BRANCH",
-                   "cv":"CIVIL ENGNEERING BRANCH",
+                   "ce":"CIVIL ENGNEERING BRANCH",
                    "ch":"CHEMICAL ENGNEERING BRANCH",
                    "bt":"BIOTECHNOLOGY BRANCH"   };
-    var years = {"16":"2k16-2k20",
-                 "17":"2k17-2k21",
-                 "18":"2k18-2k22",
-                 "19":"2k19-2k23",
-                 "20":"2k20-2k24",
-                 "21":"2k21-2k25",
-                 "22":"2k22-2k26" };
 
 
 if (in2[0] == "B" || in2[0] == "b") {
@@ -40,12 +33,17 @@ if (in2[0] == "B" || in2[0] == "b") {
 		document.getElementById("out1").textContent = "hello Mr./Ms. " + in1 + "!";
 		document.getElementById("out2").textContent = "Welcome To Nit Calicue Website.";
 
-		if (in2[2] == "6" || in2[2] == "7") {
-			document.getElementById("out3").textContent = "You Completed Your B.tech in NIT Calicut" + " the year of passed-out is:" + years[in2[1]+in2[2]];
+		if (in2[1]+in2[2] <= 17) {
+
+            var intiger = in2[1]+in2[2];
+            var num = parseInt(intiger)+ 4;
+			document.getElementById("out3").textContent = "You Completed Your B.tech in NIT Calicut" + " the year of passed-out is: " + "2k" + in2[1]+in2[2] + "-2k" + num;
 			document.getElementById("out4").textContent = "Your Completed Course is:" + groups[in2[7]+in2[8]];
 		}
 		else {
-			document.getElementById("out3").textContent = "currenty your are stydying in Nit calicut, and you are:" + years[in2[1]+in2[2]] + " Batch";
+            var intiger = in2[1]+in2[2];
+            var num = parseInt(intiger)+ 4;
+			document.getElementById("out3").textContent = "currenty your are stydying in Nit calicut, and you are:" + "2k" + in2[1]+in2[2] + "-2k" + num + " Batch";
             document.getElementById("out4").textContent = "Your Currently studying in :" + groups[in2[7]+in2[8]];
 		}
              
@@ -67,14 +65,13 @@ if (in2[0] == "B" || in2[0] == "b") {
      
     else {
     	    document.getElementById("out2").textContent = "sorry you entered invalid login details!";
-		    document.getElementById("out3").textContent = "please once again enter the correct emailid and roll number";
+		    document.getElementById("out3").textContent = "please once again enter the NITC-Registered name and roll number";
     }
 }
 
 else {
 	document.getElementById("out2").textContent = "sorry you entered invalid login details!";
-	document.getElementById("out3").textContent = "please once again enter the correct emailid and roll number";
+	document.getElementById("out3").textContent = "please once again enter the NITC-Registered name and roll number";
 	}
     
 }
-
